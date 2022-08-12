32b9e97075b3394bfe8b1c2a970676460f4f7516
aryankix101 <aryankumawat33@gmail.com>
2022-08-12 10:15:07 -0400

    First commit

---
+++ App.js
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from './components/Login.js';
+import Signup from './components/Signup.js';
+import Dashboard from './components/Dashboard.js';
+const Stack = createStackNavigator();
+function MyStack() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Signup"
+      screenOptions={{
+        headerTitleAlign: 'center',
+        headerStyle: {
+          backgroundColor: '#3740FE',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}>
+      <Stack.Screen 
+        name="Signup" 
+        component={Signup} 
+        options={{ title: 'Signup' }}
+      />       
+      <Stack.Screen 
+        name="Login" 
+        component={Login} 
+        options={
+          {title: 'Login'},
+          {headerLeft: null} 
+        }
+      />
+      <Stack.Screen 
+       name="Dashboard" 
+       component={Dashboard} 
+       options={
+         { title: 'Dashboard' },
+         {headerLeft: null} 
+       }
+      />
+    </Stack.Navigator>
+  );
+}
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
---
+++ database/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDj4ic6mgzJgFRMm_PCtobgMubWXSEYhFI",
+    authDomain: "tutorfind-d293d.firebaseapp.com",
+    projectId: "tutorfind-d293d",
+    storageBucket: "tutorfind-d293d.appspot.com",
+    messagingSenderId: "495787610837",
+    appId: "1:495787610837:web:402075baec31cefd91c85c",
+    measurementId: "G-FMQQ8WLKEK"
+  };
+  
+firebase.initializeApp(firebaseConfig);
+export default firebase;
